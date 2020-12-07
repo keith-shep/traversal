45f5e867dfcf0c9887f222787d1b1f70e901c245
Keith Shepherdson <kmson7@gmail.com>
2020-12-07 17:55:07 +0800

    enable sign-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
-         #:registerable,
+         :recoverable, :rememberable, :validatable,
+         :registerable
 end
