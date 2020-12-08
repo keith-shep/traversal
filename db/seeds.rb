32b0354e1e04ae240f80f4b666de3bbf2d9cd326
Keith Shepherdson <kmson7@gmail.com>
2020-12-08 16:47:46 +0800

    add raw seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# x + 1 = 3
+# x = 2
+
+# 2x = 4
+# x = 2
+
+### pg 52, eg 2, (ii)
+# 3^{2n}\times 15^{3n}\div 5^n
+# 3^{2n}\times 3^{3n}\times 5^{3n}\div 5^n
+# 3^{5n}\times 5^{2n}
+
+### pg 52, eg 3
+# \sqrt{8}\div \sqrt{2}
+# \sqrt{\frac{8}{2}}
+# \sqrt{4}
+# 2
+
+### pg 55, eg 5
+# \left(\sqrt{3}+3\sqrt{2}\right)\left(\sqrt{3}-3\sqrt{2}\right)
+# \left(\sqrt{3}\right)^2-9\left(\sqrt{2}\right)^2
+# 3-9\left(2\right)
+# -19
