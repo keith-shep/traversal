9adb2cfe0b1d73e87a4fa955300532c8e9e1dd37
tom chang <tomlin83@gmail.com>
2020-12-08 11:36:48 +0800

    added create in Equations controller

--- app/controllers/equations_controller.rb
+++ app/controllers/equations_controller.rb
@@ -1,2 +1,14 @@
 class EquationsController < ApplicationController
+  def create
+    @equation = Equation.new(equation_params)
+    @user = User.find(params[user_id])
+
+  end
+
+private
+
+def equation_params
+  params.require(:equation).permit(:title)
+end
+
 end
