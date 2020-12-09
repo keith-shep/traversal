6e9075d3fb0c89469707ba611d51865c856c3e46
Keith Shepherdson <kmson7@gmail.com>
2020-12-09 17:32:23 +0800

    latex create and read work

--- app/javascript/plugins/init_mathquill.js
+++ app/javascript/plugins/init_mathquill.js
@@ -3,9 +3,11 @@ import MathQuill from 'mathquill';
 const initMathQuill = () => {
   // var latexSpan = document.getElementById('latex');
 
-
   const MQ = MathQuill.getInterface(2); // for backcompat
   const mathFieldSpan = document.getElementById('mathquill-editable');
+
+  const steps = document.querySelectorAll('.step');
+  steps.forEach((step) => MQ.StaticMath(step));
 
   const mathField = MQ.MathField(mathFieldSpan, {
     spaceBehavesLikeTab: true, // configurable
@@ -15,7 +17,6 @@ const initMathQuill = () => {
         const input = document.querySelector('#step_latex');
         const latexStr = mathField.latex()
         input.value = latexStr
-
       }
     }
   });
