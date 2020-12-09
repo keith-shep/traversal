5c5f7e926a29083cc16cc5293b867cf530d78954
Keith Shepherdson <kmson7@gmail.com>
2020-12-09 16:47:40 +0800

    save

--- app/javascript/plugins/init_mathquill.js
+++ app/javascript/plugins/init_mathquill.js
@@ -1,18 +1,25 @@
 import MathQuill from 'mathquill';
 
 const initMathQuill = () => {
-  var mathFieldSpan = document.getElementById('math-field');
-  var latexSpan = document.getElementById('latex');
+  // var latexSpan = document.getElementById('latex');
 
-  var MQ = MathQuill.getInterface(2); // for backcompat
-  var mathField = MQ.MathField(mathFieldSpan, {
+
+  const MQ = MathQuill.getInterface(2); // for backcompat
+  const mathFieldSpan = document.getElementById('mathquill-editable');
+
+  const mathField = MQ.MathField(mathFieldSpan, {
     spaceBehavesLikeTab: true, // configurable
     handlers: {
-      edit: function() { // useful event handlers
-        latexSpan.textContent = mathField.latex(); // simple API
+      edit: function()  { // useful event handlers
+        // latexSpan.textContent = mathField.latex(); // simple API
+        const input = document.querySelector('#step_latex');
+        const latexStr = mathField.latex()
+        debugger;
       }
     }
   });
 }
 
 export { initMathQuill }
+
+
