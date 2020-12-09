2171545b9b2328315e868a8c63e40bf4dcbb0d1e
tom chang <tomlin83@gmail.com>
2020-12-09 23:23:09 +0800

    left wtih create method to sort out

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,26 @@
 class CommentsController < ApplicationController
   def new
+    @step = Step.find(params[:step_id])
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @step = Step.find(params[:step_id])
+    @comment.step = @step
+    if @comment.save
+      redirect_to equation_path(@step.equation)
+    else
+      render :new
+    end
   end
 
   def destroy
   end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message)
+  end
 end
