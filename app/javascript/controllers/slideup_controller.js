8b01bb36868eaa33909847559c36f301ca484634
tom chang <tomlin83@gmail.com>
2020-12-10 18:00:04 +0800

    inserted JS token, finished 1/2 - TBC

--- app/javascript/controllers/slideup_controller.js
+++ app/javascript/controllers/slideup_controller.js
@@ -1,7 +1,36 @@
 import { Controller } from "stimulus";
+import { fetchWithToken } from "../utils/fetch_with_token";
 
 export default class extends Controller {
+  static targets = [ 'source', 'equationId'];
+
   connect() {
-    console.log('Hello!');
+    console.log(this.sourceTarget);
+  }
+
+  submit(event) {
+    event.preventDefault();
+    console.log('Hello');
+
+    const equationId = Number.parseInt(this.equationIdTarget.value, 10);
+    const latexValue = document.getElementById('step_latex').value;
+
+    console.log(latexValue);
+    console.log(JSON.stringify({ step: { latex: latexValue }}));
+
+    fetchWithToken(`/equations/${equationId}/steps`, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ step: { latex: latexValue }})
+    })
+      .then(response => response.json())
+      .then((data) => {
+        // handle JSON response from server
+        console.log('yay');
+        console.log(data);
+      });
   }
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,9 +25,9 @@ require("channels")
 // External imports
 import "bootstrap";
 
-import { initMathQuill } from './../plugins/init_mathquill.js'
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initMathQuill } from './../plugins/init_mathquill.js'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
