011fcb87d79e3b4e9ce950d0ca830688f0192152
tom chang <tomlin83@gmail.com>
2020-12-14 16:25:03 +0800

    showed comments

--- app/javascript/plugins/init_tippy_in_equations_page.js
+++ app/javascript/plugins/init_tippy_in_equations_page.js
@@ -2,13 +2,16 @@
 import tippy from 'tippy.js';
 
 const loadPopOvers = () => {
-  const comments = document.getElementById('comments');
+  // const comments = document.getElementById('comments');
   // debugger;
-  tippy('#popover_comments', {
-     content: console.log(comments),
-     // content: console.log("hey"),
-     allowHTML: true,
-    });
+  // const steps = document.querySelectorAll('.step.text-dark');
+
+  const allSteps = tippy(document.querySelectorAll('.step.text-dark'));
+  allSteps.forEach((step) => {
+    let template = document.querySelector(`template[data-step-id="${step.reference.dataset.stepId}"]`)
+    step.setProps({allowHTML: true})
+    step.setContent(template.innerHTML);
+  })
 }
 
 export { loadPopOvers }
