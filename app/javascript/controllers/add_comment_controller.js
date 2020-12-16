7669b78db22895dcfe6e76d715db37179919d37c
tom chang <tomlin83@gmail.com>
2020-12-16 19:06:21 +0800

    almost there with the tippies;

--- app/javascript/controllers/add_comment_controller.js
+++ app/javascript/controllers/add_comment_controller.js
@@ -1,12 +1,19 @@
 import { Controller } from "stimulus";
 import { fetchWithToken } from "../utils/fetch_with_token";
 
+import tippy from 'tippy.js';
+import 'tippy.js/themes/light-border.css';
+import 'tippy.js/animations/shift-away.css';
+import 'tippy.js/dist/svg-arrow.css';
 
 export default class extends Controller {
   static targets = [ 'title', 'newTitle', 'step', 'toggleForm'];
+  allSteps = [];
+
 
   connect() {
     console.log('I am the add Comment Controller');
+    this.loadPopOvers()
   }
 
   submit(e) {
@@ -15,9 +22,6 @@ export default class extends Controller {
     const stepId = this.stepTarget.dataset.step;
     console.log(newTitle);
     console.log(stepId);
-
-
-
 
     fetchWithToken(`/steps/${stepId}/comments`, {
       method: "POST",
@@ -28,14 +32,34 @@ export default class extends Controller {
       body: JSON.stringify({ comment: { message :newTitle }})
     })
       .then(response => response.json())
-      .then((data) => {
-        // handle JSON response from server
-        console.log(data);
-        this.newTitleTarget.value = "";
-        console.log(newTitle);
-        const stepCommentTemplate = document.querySelector(`template[data-step-id="${stepId}"] div.step-comments`);
-        stepCommentTemplate.insertAdjacentHTML("beforeend", `<ul style='list-style: none'> <li>${newTitle}</li>  </ul> `);
-      });
+      .then((commentData) => this.updateTippy(commentData));
   }
+
+  updateTippy(commentData) {
+    console.log(commentData);
+    this.newTitleTarget.value = "";
+    let targetStep = this.allSteps.find((step) => {return step.reference.dataset.tippyId == commentData.comment.step_id })
+    // const stepCommentTemplate = document.querySelector(`template[data-step-id="${stepId}"]`);
+    // const commentList = stepCommentTemplate.content.querySelector('.step-comments')
+    // commentList.insertAdjacentHTML("beforeend", `<ul style='list-style: none'> <li>${newTitle}</li>  </ul> `);
+    // debugger
+  }
+
+  loadPopOvers() {
+    this.allSteps = tippy(document.querySelectorAll('.step.text-dark'), {
+      theme: 'light-border',
+      animation: 'shift-away',
+      placement: 'bottom',
+    });
+
+    this.allSteps.forEach((step) => {
+
+     let template = document.querySelector(`template[data-step-id="${step.reference.dataset.tippyId}"]`)
+
+      step.setProps({allowHTML: true})
+      step.setContent(template.innerHTML);
+    })
+  }
+
 
 }
