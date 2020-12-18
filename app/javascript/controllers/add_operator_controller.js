49ca22300ca0bb0c573671acc164733d0de934f7
miguel jimenez <denkatalabeno@gmail.com>
2020-12-18 08:21:51 +0800

    addding elements manually now breaks mq

--- app/javascript/controllers/add_operator_controller.js
+++ app/javascript/controllers/add_operator_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "stimulus"
+import { mathQuillCommands } from "../utils/mathquill_commands.js"
 
 export default class extends Controller {
   static targets = [ ]
@@ -8,7 +9,13 @@ export default class extends Controller {
   }
 
   call(e) {
-    console.log('calling ya');
+    let inputLatex = document.querySelector('#step_latex');
+    let rootMQ = document.querySelector('#mathquill-editable > .mq-root-block')
+    // within the root -> inject the relevant html
+
+    rootMQ.insertAdjacentHTML('beforeend', mathQuillCommands[`${e.currentTarget.dataset.mqId}`].html);
+    // update the input value WITH THE ACTUAL STRING
+    inputLatex.value += `${mathQuillCommands[e.currentTarget.dataset.mqId].value}`
   }
 
 }
