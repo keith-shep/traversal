a2f63aff0272dc6975c714b27da4b49c449ec36b
miguel jimenez <denkatalabeno@gmail.com>
2020-12-18 09:33:45 +0800

    adding an operator works now - manipulating the MQ instance directly instead

--- app/javascript/controllers/add_operator_controller.js
+++ app/javascript/controllers/add_operator_controller.js
@@ -1,21 +1,55 @@
-import { Controller } from "stimulus"
-import { mathQuillCommands } from "../utils/mathquill_commands.js"
+import { Controller } from "stimulus";
+import MathQuill from 'mathquill';
 
 export default class extends Controller {
   static targets = [ ]
 
   connect() {
-    console.log('collapsable is connected!');
+    this.initMathQuill();
   }
 
-  call(e) {
-    let inputLatex = document.querySelector('#step_latex');
-    let rootMQ = document.querySelector('#mathquill-editable > .mq-root-block')
-    // within the root -> inject the relevant html
+  // call(e) {
+  //   let inputLatex = document.querySelector('#step_latex');
+  //   let rootMQ = document.querySelector('#mathquill-editable > .mq-root-block')
+  //   // within the root -> inject the relevant html
 
-    rootMQ.insertAdjacentHTML('beforeend', mathQuillCommands[`${e.currentTarget.dataset.mqId}`].html);
-    // update the input value WITH THE ACTUAL STRING
-    inputLatex.value += `${mathQuillCommands[e.currentTarget.dataset.mqId].value}`
+  //   rootMQ.insertAdjacentHTML('beforeend', mathQuillCommands[`${e.currentTarget.dataset.mqId}`].html);
+  //   // update the input value WITH THE ACTUAL STRING
+  //   inputLatex.value += `${mathQuillCommands[e.currentTarget.dataset.mqId].value}`
+  // }
+
+
+
+
+  initMathQuill() {
+  // var latexSpan = document.getElementById('latex');
+  const MQ = MathQuill.getInterface(2); // for backcompat
+  const mathFieldSpan = document.getElementById('mathquill-editable');
+
+  const steps = document.querySelectorAll('.step');
+  steps.forEach((step) => MQ.StaticMath(step));
+
+  const mathField = MQ.MathField(mathFieldSpan, {
+    spaceBehavesLikeTab: true, // configurable
+    handlers: {
+      edit: function()  { // useful event handlers
+        const input = document.querySelector('#step_latex');
+        const latexStr = mathField.latex()
+        input.value = latexStr
+      }
+    }
+    });
+  this.addShortcuts(mathField)
+  }
+
+  addShortcuts(mathField) {
+    const allShortCutsTable = document.querySelectorAll('tbody > tr');
+    allShortCutsTable.forEach((tr) => {
+      debugger
+      tr.addEventListener('click', () => {
+        mathField.write(tr.cells[0].innerText);
+      })
+    })
   }
 
 }
